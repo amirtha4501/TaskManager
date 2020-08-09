de09c85c9cffc1bc7c8db3ef653e734edcfb19fb
Amirtha <amirtha4501@gmail.com>
2020-08-09 23:03:51 +0530

    Updates

---
+++ precache-manifest.cf252133dbb9e73f0ed66309674acbab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f93571c1f0027163597f73455967bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1aed2e74c2b85699772",
+    "url": "/static/css/main.c4835130.chunk.css"
+  },
+  {
+    "revision": "fe3f5b47c846facb37fe",
+    "url": "/static/js/2.b06b4890.chunk.js"
+  },
+  {
+    "revision": "a1aed2e74c2b85699772",
+    "url": "/static/js/main.b5d53643.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
